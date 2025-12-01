73d95c757861562c39a27fd472bccbaa1acb33ad
roky18 <rokymax66@gmail.com>
2025-12-02 03:06:26 +0600

    part-7 v-2 finish

--- index.js
+++ index.js
@@ -173,6 +173,20 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/parcels/rider", async (req, res) => {
+      const { riderEmail, deliveryStatus } = req.query;
+      const query = {};
+      if (riderEmail) {
+        query.riderEmail = riderEmail;
+      }
+      if (deliveryStatus) {
+        query.deliveryStatus = deliveryStatus;
+      }
+      const cursor = parcelsCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
     app.get("/parcels/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
@@ -202,7 +216,7 @@ async function run() {
 
       const updateDoc = {
         $set: {
-          deliveryStatus: "Driver_Assigned",
+          deliveryStatus: "driver_assigned",
           riderId: riderId,
           riderName: riderName,
           riderEmail: riderEmail,
